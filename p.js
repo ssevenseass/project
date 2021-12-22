d4474e1ee38d41132bcb39fdd96343182618d931
Kristina <kristinakuksenko@mail.ru>
2021-12-23 01:44:59 +0700

    add

--- p.js
+++ p.js
@@ -1,11 +1,13 @@
 const name = document.getElementById('email');
 const pw = document.getElementById('password');
+const lg = document.getElementById('login')
 const btn = document.getElementById('my_button')
 btn.onclick = function() {
     event.preventDefault();
     console.log(name.value)
     localStorage.setItem('email', name.value);
     localStorage.setItem('password', pw.value);
+    localStorage.setItem('login',lg.value )
     document.location.href = "index.html"
 }
 
